318d2ec2d31b4658abf431ca04dad662d1da999b
Kyumin Kim <68532437+1006lem@users.noreply.github.com>
2023-05-24 23:45:02 +0900

    kyumin-upload

---
+++ BeMyPlant/app/src/main/java/com/example/bemyplant/SettingActivity.kt
@@ -0,0 +1,59 @@
+package com.example.bemyplant
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class SettingActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.menu_bottom_navigation)
+
+        bottomNavigationView.selectedItemId = R.id.menu_setting
+        val menuView = bottomNavigationView.getChildAt(0) as BottomNavigationMenuView
+//        val selectedItemIndex = 3
+//
+//        menuView.getChildAt(selectedItemIndex).performClick()
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    // "홈" 메뉴 클릭 시 MainActivity로 이동
+                    val homeIntent = Intent(this@SettingActivity, MainActivity::class.java)
+                    startActivity(homeIntent)
+                    true
+                }
+
+                R.id.menu_setting -> {
+                    // "setting" 메뉴 클릭 시 SettingActivity로 이동
+                    val boardIntent = Intent(this@SettingActivity, SettingActivity::class.java)
+                    startActivity(boardIntent)
+                    true
+                }
+
+                R.id.menu_chat -> {
+                    // "채팅" 메뉴 클릭 시 ChatActivity로 이동
+                    val chatIntent = Intent(this@SettingActivity, ChatActivity::class.java)
+                    startActivity(chatIntent)
+                    true
+                }
+
+                R.id.menu_diary -> {
+                    // "일기" 메뉴 클릭 시 DiaryActivity로 이동
+                    val diaryIntent = Intent(this@SettingActivity, DiaryActivity::class.java)
+                    startActivity(diaryIntent)
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+}
