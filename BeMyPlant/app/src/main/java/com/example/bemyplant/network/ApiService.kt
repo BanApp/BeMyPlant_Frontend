e172e79df563ac7b45bc8ee6e78a03531a52a89b
Kyumin Kim <68532437+1006lem@users.noreply.github.com>
2023-07-03 07:13:00 +0900

    ~캡스톤 발표(~로그인)

--- BeMyPlant/app/src/main/java/com/example/bemyplant/network/ApiService.kt
+++ BeMyPlant/app/src/main/java/com/example/bemyplant/network/ApiService.kt
@@ -1,11 +1,15 @@
 package com.example.bemyplant.network
+import com.example.bemyplant.data.GardenResponse
 import com.example.bemyplant.data.LoginData
 import com.example.bemyplant.data.SignUpData
+import com.example.bemyplant.data.SensorData
 import com.example.bemyplant.data.LoginResponse
 import com.example.bemyplant.data.SignUpResponse
 import retrofit2.Response
 import retrofit2.http.POST
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
     @POST("api/authenticate")
@@ -16,6 +20,17 @@ interface ApiService {
 
     @POST("api/signup")
     suspend fun signUp(@Body signUpData: SignUpData): Response<SignUpResponse>
+
+    @GET("api/get-sensor-data")
+    //suspend fun getSensorData(@Query("num") num: Int): Response<SensorsData>
+    suspend fun getSensorData(@Query("num") num: Int): Response<ArrayList<SensorData>>
+
+
+    @GET("garden/gardenDtl")
+    //suspend fun getSensorData(@Query("num") num: Int): Response<SensorsData>
+    suspend fun getGardenDetail(@Query("apiKey") apiKey: String,@Query("cntntsNo") cntntsNo: String): Response<GardenResponse>
+
+
 
 }
 
