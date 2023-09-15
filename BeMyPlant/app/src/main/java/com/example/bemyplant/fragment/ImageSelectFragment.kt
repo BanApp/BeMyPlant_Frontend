764bbe0228084da67fc84996bc816e02597873f3
Kyumin Kim <68532437+1006lem@users.noreply.github.com>
2023-09-15 23:25:26 +0900

    이미지 reselect 코드 수정 (이슈 #14 참고)

--- BeMyPlant/app/src/main/java/com/example/bemyplant/fragment/ImageSelectFragment.kt
+++ BeMyPlant/app/src/main/java/com/example/bemyplant/fragment/ImageSelectFragment.kt
@@ -23,17 +23,12 @@ private const val ARG_PARAM2 = "param2"
  */
 class ImageSelectFragment : Fragment() {
     var selectedImage: String = ""
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
     val binding by lazy{FragmentImageSelectBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-// 이미지 선택하면 문자열이 넘어감.
+
+        // 이미지 선택하면 문자열이 넘어감.
         binding.imageButtonImageSelectPlant1.setOnClickListener{
             selectedImage = "flower1"
             val intent = Intent(requireActivity(), MainActivity::class.java)
