b26fec65b30d07c9162f23239620aa09dbe269d1
Kyumin Kim <68532437+1006lem@users.noreply.github.com>
2023-09-24 22:00:12 +0900

    IDcard, flower-reselect combining..

--- BeMyPlant/app/src/main/java/com/example/bemyplant/fragment/FlowerIdFragment.kt
+++ BeMyPlant/app/src/main/java/com/example/bemyplant/fragment/FlowerIdFragment.kt
@@ -1,41 +1,21 @@
 package com.example.bemyplant.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.bemyplant.R
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.fragment.app.FragmentTransaction
-import android.content.Intent
+import com.example.bemyplant.R
 
 
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FlowerIdFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FlowerIdFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class FlowerIdFragment(bundle: Bundle) : Fragment() {
+    private lateinit var plantImage: ImageView
+    private lateinit var plantName: TextView
+    private lateinit var plantRegistration: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,13 +23,31 @@ class FlowerIdFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_flower_id, container, false)
         val mainFlower = rootView.findViewById<ImageButton>(R.id.mainFlower)
+        val plantRace = rootView.findViewById<TextView>(R.id.plantVariety)
+        val plantName = rootView.findViewById<TextView>(R.id.flowerName)
+        val plantRegistration = rootView.findViewById<TextView>(R.id.plantRegistration)
+
+        val bundle = arguments
+        if (bundle != null && bundle.containsKey("plantImage") && bundle.containsKey("plantName") &&  bundle.containsKey("plantRace") && bundle.containsKey("plantRegistration")) {
+            mainFlower.setImageBitmap(bundle.getParcelable("plantImage"))
+            plantName.text = bundle.getString("plantName")
+            plantRace.text = bundle.getString("plantRace")
+            plantRegistration.text = bundle.getString("plantRegistration")
+        }
+        else{
+            // TODO: (정현) 식물 DB 조회
+            //      식물 이미지, 이름, 품종, 주민 등록번호 가져와서 렌더링 (mainFlower, flowerName, plantRace,  plantRegistration)
+
+        }
 
         mainFlower.setOnClickListener{
             val flagValue = true
-            val imageSelectFragment = ImageSelectFragment.newInstance("param1", "param2", flagValue)
+            //val imageSelectFragment = ImageSelectFragment.newInstance(flagValue, plantName.text.toString(), plantRace.text.toString())
+            val imageSelectFragment = ImageSelectFragment.newInstance(flagValue, "", "")
             val transaction = requireActivity().supportFragmentManager.beginTransaction()
             val sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
-            sharedViewModel.imageText = "뽀삐를\n다시 선택해보아요!"
+
+            sharedViewModel.imageText =  plantName.text.toString() + "을(를)\n다시 선택해보아요!"
             transaction.replace(android.R.id.content, imageSelectFragment)
             transaction.addToBackStack(null)
             transaction.commit()
@@ -59,24 +57,4 @@ class FlowerIdFragment : Fragment() {
         return rootView
     }
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FlowerIdFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FlowerIdFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
