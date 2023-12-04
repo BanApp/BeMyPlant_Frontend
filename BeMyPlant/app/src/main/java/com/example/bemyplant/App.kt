30c992e8cf39213e0b43e77c523c9b220e18aa9f
Kyumin Kim <68532437+1006lem@users.noreply.github.com>
2023-12-04 13:55:52 +0900

    update APP (AUTO LOGIN using TOKEN)

--- BeMyPlant/app/src/main/java/com/example/bemyplant/App.kt
+++ BeMyPlant/app/src/main/java/com/example/bemyplant/App.kt
@@ -1,12 +1,25 @@
 package com.example.bemyplant
 
 import android.app.Application
+import android.content.Context
+import android.content.Intent
 import io.realm.Realm
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+
+        val sharedPreferences = getSharedPreferences("Prefs", Context.MODE_PRIVATE)
+        val token: String? = sharedPreferences.getString("token", null)
+
+        if (token == null) {
+            // token이 없을 때 처리
+            val intent = Intent(this, MJ_MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
 
         // Initialize Realm. Should only be done once when the application starts.
         //val realmConfig : RealmConfiguration = RealmConfiguration.Builder().build();
